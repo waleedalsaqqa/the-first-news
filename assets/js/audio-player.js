f6890de52066b0cdf29c747a885acddb0a5491f7
blackzetso <engelshafiy6@gmail.com>
2024-12-01 21:41:12 +0200

    first commit

---
+++ assets/js/audio-player.js
@@ -0,0 +1,12 @@
+jQuery(document).ready(function($){var presetBrowser={globals:{currentAudioGallery:null,stickyplayer:{status:null}}}
+init();function init(){initAudioPlayer();document.addEventListener('mySuperEvt',function(e){console.log("mySuperEvt capture on document level");document.getElementById(presetBrowser.globals.currentAudioGallery).api_goto_next();},false);}
+function initAudioPlayer(){var settings_ap={autoplayNext:true,autoplay:'true',disable_scrub:'default',design_skin:'skin-wave',skinwave_dynamicwaves:'on',cue:'on',transition:'fade'};var ag_id="#audio-gallery-0";var gallery=dzsag_init(ag_id,{'transition':'fade','autoplay':'off','settings_ap':settings_ap});$("audio",ag_id).on("ended",function(){console.log("audio audiogallery jQuery .on('ended')");document.getElementById(presetBrowser.globals.currentAudioGallery).api_goto_next();});initStickyPlayer(this);}
+function initStickyPlayer(audioGal){var settings_ap={autoplayNext:true,swf_location:"/templates/arturia-bootstrap/assets/scripts/jquery-plugins/waveform-audio-player/ap.swf",autoplay:'true',preload_method:'none',disable_scrub:'default',design_skin:'skin-wave',skinwave_dynamicwaves:'on',cue:'on',transition:'fade',skinwave_mode:'small'};dzsap_init('.stickyplayer',settings_ap);if(presetBrowser.globals.stickyplayer.status!="initialized_almost_once")
+{$('.stickyplayer .btn-prev').on('click',function(event){document.getElementById(presetBrowser.globals.currentAudioGallery).api_goto_prev();})
+$('.stickyplayer .btn-next').on('click',function(event){document.getElementById(presetBrowser.globals.currentAudioGallery).api_goto_next();})}
+$('.stickyplayer audio')[0].addEventListener('ended',function(event){console.log("stickyplayer ended captured from addEventListern");})
+$('.stickyplayer audio')[0].onended=function(event){console.log("stickyplayer ended captured from onended");}
+$(".stickyplayer audio").on("ended",function(){console.log("stickyplayer jQuery .on('ended')");document.getElementById(presetBrowser.globals.currentAudioGallery).api_goto_next();})
+presetBrowser.globals.stickyplayer.status="initialized_almost_once";}
+function showStickyPlayer(audiogaleryID){var packname=$('.playerInfos',audiogaleryID).data('packname');$(".dzsap-sticktobottom .the-artist").html(packname);$(".dzsap-sticktobottom").animate({opacity:1},700,function(){$(window).trigger('resize');});}
+function hideStickyPlayer(){$(".dzsap-sticktobottom").css({"opacity":"0"});}});
